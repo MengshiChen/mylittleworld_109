f8ffd48dcb3f13e47def79ccccbdc35e044f2e0f
MengshiChen <moussechen0205@gmail.com>
2021-12-13 22:10:46 -0800

    three.module.js

--- public/src/GLTFLoader.js
+++ public/src/GLTFLoader.js
@@ -61,7 +61,7 @@ import {
 	Vector3,
 	VectorKeyframeTrack,
 	sRGBEncoding
-} from 'https://mengshichen.github.io/public/build/three.module.js';
+} from '/public/build/three.module.js';
 
 class GLTFLoader extends Loader {
 
